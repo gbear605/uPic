20d16208767a618af5fce0c7a754859e83961d84
gbear605 <garrisontaylor@gmail.com>
2014-02-22 10:40:38 -0500

    Get setup with Ruby On Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UPic::Application.config.secret_key_base = '4c2c679e5f0ec92629ae45baa1dafa1a884961df396443856afaf0a795ed632748ab3cb044d34b0edbd3e0b086d88fd22e5a59e0aaf2a14cc61df6ced9c1e5ab'
